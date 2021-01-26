4ae11c6a1d164f404140bc611100858f2293c8a6
Gordon Child <gordon@gordonchild.com>
2021-01-25 23:05:12 -0600

    add pam-watchid formula

---
+++ pam_watchid.rb
@@ -0,0 +1,17 @@
+class PamWatchid < Formula
+  desc "PAM plugin module that allows the Apple Watch to be used for authentication"
+  homepage "https://github.com/gordolio/pam-watchid"
+  head "https://github.com/gordolio/pam-watchid.git"
+
+  def install
+    system "make", "install", "PREFIX=#{prefix}"
+  end
+
+  def caveats; <<~EOS
+    Make sure you add the module to your targeted service in /etc/pam.d/:
+      auth  sufficient  pam_watchid.so
+      ...
+    See https://github.com/gordolio/pam-watchid
+  EOS
+  end
+end
